c6dd9d000943fef262a8a3a564e0928580d27026
matyshinaj <genya2001.6@mail.ru>
2022-03-06 02:32:49 +0300

    Added homework 2

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTests.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTests.java
@@ -1,16 +1,36 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import androidx.annotation.NonNull;
+import android.view.View;
+import android.widget.ListView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anyOf;
 
 @RunWith(AndroidJUnit4.class)
 public class EspressoTests {
@@ -27,5 +47,51 @@ public class EspressoTests {
     @Test
     public void checkPositive() {
         onView(withId(R.id.text_home)).check(ViewAssertions.matches(withText("This is home fragment")));
+    }
+
+    @Test
+    public void testIntent() {
+        Intents.init();
+        try {
+            openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        } catch (Exception e) {
+        }
+        onView(anyOf(withText(R.string.action_settings), withId(R.id.action_settings))).perform(click());
+        Intents.intended(hasData("https://google.com"));
+    }
+
+    @Before
+    public void registerIdlingResources() { //Подключаемся к “счетчику”
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After // Выполняется после тестов
+    public void unregisterIdlingResources() { //Отключаемся от “счетчика”
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+
+    @Test
+    public void testGalleryList() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_gallery)).perform(click());
+        onView(withId(R.id.recycle_view)).check(ViewAssertions.matches (CustomMatchers.withListSize(10)));
+    }
+}
+
+class CustomMatchers {
+
+    public static Matcher<View> withListSize (final int size) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely (final View view) {
+                RecyclerView.Adapter adapter = ((RecyclerView) view).getAdapter();
+                return adapter.getItemCount() == size;
+            }
+
+            @Override
+            public void describeTo (final Description description) {
+                description.appendText ("RecyclerView should have " + size + " items");
+            }
+        };
     }
 }
