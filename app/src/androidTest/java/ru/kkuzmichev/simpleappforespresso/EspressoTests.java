44c374e39c9e97b9aed7064cad30df761568836d
matyshinaj <genya2001.6@mail.ru>
2022-03-13 21:43:26 +0300

    Added homework 3

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTests.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTests.java
@@ -1,6 +1,9 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import androidx.annotation.NonNull;
+
+import android.Manifest;
+import android.os.Environment;
 import android.view.View;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ApplicationProvider;
@@ -11,6 +14,9 @@ import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -18,6 +24,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -28,12 +35,39 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.internal.platform.util.TestOutputEmitter.takeScreenshot;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static kotlin.jvm.internal.Intrinsics.checkNotNull;
 import static ru.kkuzmichev.simpleappforespresso.CustomMatchers.itemVisible;
 import static org.hamcrest.Matchers.anyOf;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class EspressoTests {
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, org.junit.runner.Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+    };
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
 
     @Rule
     @NonNull
@@ -41,16 +75,19 @@ public class EspressoTests {
 
     @Test
     public void checkNegative() {
+        Allure.feature("checkNegative");
         onView(withId(R.id.text_home)).check(matches(withText("BLA BLA BLA")));
     }
 
     @Test
     public void checkPositive() {
+        Allure.feature("checkPositive");
         onView(withId(R.id.text_home)).check(matches(withText("This is home fragment")));
     }
 
     @Test
     public void testIntent() {
+        Allure.feature("testIntent");
         Intents.init();
         try {
             openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
@@ -62,26 +99,45 @@ public class EspressoTests {
 
     @Before
     public void registerIdlingResources() { //Подключаемся к “счетчику”
+        Allure.feature("registerIdlingResources");
         IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
     }
 
     @After // Выполняется после тестов
     public void unregisterIdlingResources() { //Отключаемся от “счетчика”
+        Allure.feature("unregisterIdlingResources");
         IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
     }
 
     @Test
     public void testGalleryList() {
+        Allure.feature("testGalleryList");
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_gallery)).perform(click());
         onView(withId(R.id.recycle_view)).check(matches (CustomMatchers.withListSize(10)));
     }
     @Test
     public void testItemElementDisplayed() {
+        Allure.feature("testItemElementDisplayed");
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_gallery)).perform(click());
         ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
         recyclerView.check(matches(itemVisible(0, isDisplayed())));
+    }
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/screenshots/");
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
 
